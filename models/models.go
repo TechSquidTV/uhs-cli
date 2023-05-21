d39b446afdc25f5504346f60b2f7ba533eaf2a3d
KyleTryon <Kyle.Tryon@circleci.com>
2023-05-21 19:52:48 -0400

    feat: add radarr

--- models/models.go
+++ models/models.go
@@ -4,6 +4,7 @@ import (
 	"github.com/techsquidtv/uhs-cli/models/services"
 	"github.com/techsquidtv/uhs-cli/models/services/plex"
 	"github.com/techsquidtv/uhs-cli/models/services/qbittorrent"
+	"github.com/techsquidtv/uhs-cli/models/services/radarr"
 	"github.com/techsquidtv/uhs-cli/models/services/sonarr"
 )
 
@@ -17,10 +18,12 @@ func DefaultServiceConfig() services.ServicesConfig {
 	qbt := &qbittorrent.Qbittorrent{}
 	plex := &plex.Plex{}
 	sonarr := &sonarr.Sonarr{}
+	radarr := &radarr.Radarr{}
 
 	config["qbittorrent"] = qbt.Default()
 	config["plex"] = plex.Default()
 	config["sonarr"] = sonarr.Default()
+	config["radarr"] = radarr.Default()
 
 	return config
 }
