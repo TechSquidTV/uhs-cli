bd049081d4e866bee5a77dc414f627519050ce94
Kyle a.k.a. TechSquidTV <33272306+KyleTryon@users.noreply.github.com>
2023-05-21 13:56:49 -0400

    feat: add default command (#1)

---
+++ cmd/common/common.go
@@ -0,0 +1,27 @@
+package common
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/techsquidtv/uhs-cli/models"
+	"gopkg.in/yaml.v3"
+)
+
+func Output(filePath string, config *models.UHSConfig) error {
+	yamlConfig, err := yaml.Marshal(config)
+	if err != nil {
+		return err
+	}
+	if filePath == "" {
+		fmt.Println(string(yamlConfig))
+		return nil
+	} else {
+		err = os.WriteFile(filePath, yamlConfig, 0644)
+		if err != nil {
+			return err
+		}
+		fmt.Println("Configuration complete! Your configuration file has been saved to " + filePath + ".")
+	}
+	return nil
+}
