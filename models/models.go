7844af3ff00aa7ae615750e2138c603411dadced
KyleTryon <Kyle.Tryon@circleci.com>
2023-05-21 21:40:08 -0400

    feat: add cloudflared

--- models/models.go
+++ models/models.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"github.com/techsquidtv/uhs-cli/models/services"
+	"github.com/techsquidtv/uhs-cli/models/services/cloudflared"
 	"github.com/techsquidtv/uhs-cli/models/services/plex"
 	"github.com/techsquidtv/uhs-cli/models/services/qbittorrent"
 	"github.com/techsquidtv/uhs-cli/models/services/radarr"
@@ -21,12 +22,14 @@ func DefaultServiceConfig() services.ServicesConfig {
 	sonarr := &sonarr.Sonarr{}
 	radarr := &radarr.Radarr{}
 	sabnzbd := &sabnzbd.Sabnzbd{}
+	cloudflared := &cloudflared.Cloudflared{}
 
 	config["qbittorrent"] = qbt.Default()
 	config["plex"] = plex.Default()
 	config["sonarr"] = sonarr.Default()
 	config["radarr"] = radarr.Default()
 	config["sabnzbd"] = sabnzbd.Default()
+	config["cloudflared"] = cloudflared.Default()
 
 	return config
 }
