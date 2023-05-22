09ef09708a48a058c81e675df8b66ebc3d156cf9
KyleTryon <Kyle.Tryon@circleci.com>
2023-05-21 22:00:11 -0400

    feat: add autobrr

---
+++ models/services/autobrr/autobrr.go
@@ -0,0 +1,54 @@
+package autobrr
+
+import (
+	"fmt"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/techsquidtv/uhs-cli/models/services"
+)
+
+type Autobrr struct {
+	services.Service `yaml:",inline"`
+	AutobrrOptions    `yaml:",inline"`
+}
+
+type AutobrrOptions struct {
+	Config    string `yaml:"config"`
+}
+
+// Return default values for service
+func (s *Autobrr) Default() services.ServiceInterface {
+	http := 7474
+
+	p := &Autobrr{
+		Service: services.Service{
+			Enabled:      false,
+			ReplicaCount: 1,
+			Image: services.Image{
+				Repository: "ghcr.io/autobrr/autobrr",
+				Tag:        "latest",
+				PullPolicy: "Always",
+			},
+			Ports: services.Ports{
+				Http: &http,
+			},
+		},
+		AutobrrOptions: AutobrrOptions{
+			Config:    "/opt/autobrr/config",
+		},
+	}
+	return p
+}
+
+func (s *Autobrr) Configure() services.ServiceInterface {
+	s.Enabled = true
+	inputConfigPath := &survey.Input{
+		Message: "Enter the path to your Autobrr config folder:",
+		Default: s.Config,
+	}
+	err := survey.AskOne(inputConfigPath, &s.Config)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return s
+}
