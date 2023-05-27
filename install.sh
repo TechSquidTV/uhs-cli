8735facf64339a611be92fe37a167bff31ebeedf
KyleTryon <Kyle.Tryon@circleci.com>
2023-05-27 19:39:38 -0400

    fix: improve install path selection

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 # Get the latest version
 VERSION=$(curl -s https://api.github.com/repos/TechSquidTV/uhs-cli/releases/latest | grep tag_name | cut -d '"' -f 4)
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
 # The operating system and architecture
 OS=$(uname)
@@ -16,6 +18,9 @@ mkdir uhs-cli_${OS}_${ARCH}
 tar -xvzf uhs-cli_${OS}_${ARCH}.tar.gz -C uhs-cli_${OS}_${ARCH}
 
 # Move the binary to a location in your PATH
+if [ ! -d ~/.local/bin ]; then
+  mkdir -p ~/.local/bin
+fi
 mv uhs-cli_${OS}_${ARCH}/uhs-cli ~/.local/bin/uhs
 
 # Ensure the binary is executable
@@ -27,3 +32,8 @@ rm -rf uhs-cli_${OS}_${ARCH}
 
 echo "Installed uhs-cli version ${VERSION}"
 echo "Run 'uhs --help' to get started"
+echo 
+# Check if the binary is in your PATH
+if ! command -v uhs &> /dev/null; then
+  echo "${RED}[WARNING]${NC} uhs could not be found in your PATH. Please add '~\${HOME}/.local/bin/' to your PATH."
+fi
