e003012738f434a41f174f449947eae6f616fe21
KyleTryon <Kyle.Tryon@circleci.com>
2023-05-31 22:29:58 -0400

    feat: add udp port name

--- models/services/services.go
+++ models/services/services.go
@@ -3,6 +3,7 @@ package services
 type Ports struct {
 	Http   *int `yaml:"http,omitempty"`
 	Https  *int `yaml:"https,omitempty"`
+	Udp    *int `yaml:"udp,omitempty"`
 	P2p    *int `yaml:"p2p,omitempty"`
 	P2pudp *int `yaml:"p2pudp,omitempty"`
 }
