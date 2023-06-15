e1e42597fc300b85182fcbcd53fd9d5e23a7539a
KyleTryon <Kyle.Tryon@circleci.com>
2023-06-14 22:38:54 -0400

    test: test unmarshalling existing configs

---
+++ cmd/common/common_test.go
@@ -0,0 +1,27 @@
+package common
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/techsquidtv/uhs-cli/models/config"
+	configCommon "github.com/techsquidtv/uhs-cli/models/common"
+	"github.com/techsquidtv/uhs-cli/models/service"
+	"github.com/techsquidtv/uhs-cli/models/service/servicemap"
+	"gopkg.in/yaml.v3"
+	"testing"
+)
+
+func TestInput(t *testing.T) {
+	// Generate sample config for a service
+	uhsConfig := config.Config{
+		Common:   new(configCommon.Common).Default(),
+		Services: make(service.ServicesConfig),
+	}
+	// Add a service to the config
+	uhsConfig.Services["plex"] = servicemap.ServiceMap["plex"].Default()
+	// Marshal the config to yaml
+	yamlConfig, err := yaml.Marshal(uhsConfig)
+	assert.NoError(t, err)
+	// Unmarshal the yaml back into a config
+	err = yaml.Unmarshal(yamlConfig, &uhsConfig)
+	assert.NoError(t, err)
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,11 @@ go 1.19
 require github.com/AlecAivazis/survey/v2 v2.3.6
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+)
+
+require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/mattn/go-colorable v0.1.2 // indirect
@@ -12,6 +17,7 @@ require (
 	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
 	github.com/spf13/cobra v1.7.0
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/testify v1.8.4
 	golang.org/x/sys v0.0.0-20220422013727-9388b58f7150 // indirect
 	golang.org/x/term v0.0.0-20210503060354-a79de5458b56 // indirect
 	golang.org/x/text v0.3.3 // indirect
